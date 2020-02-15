04ae1a133bbe9847f0393c614b688798f6f076e5
John William Davis <jwdavisdeveloper@gmail.com>
2020-02-15 15:15:12 -0800

    finished this exercise

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-console.log("👀")
+
+document.addEventListener("DOMContentLoaded", function() {
+var placeholder = document.createElement("h3");
+var display = document.getElementById("display-quote").appendChild(placeholder);
+const button = document.getElementById("get-started");
+
+button.addEventListener('click', function(e) {
+        fetch("https://api.kanye.rest")
+        .then(function(response) {
+            return response.json();
+        }).then(function(jsonData) {
+            console.log(jsonData)
+            console.log(jsonData.quote);
+            display.innerHTML = jsonData.quote;
+            
+        })
+        
+        })
+});
